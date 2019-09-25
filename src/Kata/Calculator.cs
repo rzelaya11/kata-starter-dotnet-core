6bd39f969704c625dd773c77c8024b76017732b7
Ferenck Vargas <ferenck@acklenavenue.com>
2019-09-25 11:56:40 -0600

    Unknown amount of numbers passing, testing for new line as delimiter

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -13,7 +13,7 @@ namespace Kata
             }
 
             var tok = userInput.Split(",").Select(int.Parse).ToArray();
-            return tok[0] + (tok.Count()>1 ? tok[1] :0);
+            return tok.Sum();
         }
     }
 }
