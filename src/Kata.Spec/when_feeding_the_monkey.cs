2684308cd24e31db85bfeaffb1294b7850223c6d
Ricardo Zelaya <ricardo.zelaya@acklenavenue.com>
2019-09-25 11:38:27 -0600

    implementing sum with one parameter

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -30,11 +30,26 @@ namespace Kata.Spec
         static Calculator _systemUnderTest;
         static int _result;
     }
+
+    public class when_user_input_is_one_number
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("2"); };
+
+        It should_return_same_number = () => { _result.Should().Be(2); };
+        
+        private static Calculator _systemUnderTest;
+        private static int _result;
+    }
 }
 
 
 
-// 1. Given the user input is empty when calculating the sum then it should return zero.
+// 1. Given the user input is empty when calculating the sum then it should return zero.XX
 // 2. Given the user input is one number when calculating the sum then it should return the same number. (example "3" should equal 3)
 // 3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 // 4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
