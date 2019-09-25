b21c5c303c18b5e1487ffd3521311dcfde40d0ec
Ricardo Zelaya <ricardo.zelaya@acklenavenue.com>
2019-09-25 11:53:41 -0600

    create test for add more than 2 numbers

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -59,9 +59,22 @@ namespace Kata.Spec
         static Calculator _systemUnderTest;
         static int _result;
     }
+
+    public class when_user_has_more_than_two_numbers
+    {
+        Establish _context = () =>
+        {
+            _systemUnderTest = new Calculator();
+        };
+
+        Because of = () => { _result = _systemUnderTest.Add("1,2,3,4,5,6"); };
+
+        It should_do_something = () => { _result.Should().Be(21); };
+        private static Calculator _systemUnderTest;
+        private static int _result;
+    }
 }
 
-// 3. Given the user input is two numbers when calculating the sum then it should return the sum of those numbers. (example "1,2" should equal 3)
 // 4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
 // 5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
 // 6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,3 +1,6 @@
+using System.Collections.Generic;
+using System.Linq;
+
 namespace Kata
 {
     public class Calculator
@@ -9,7 +12,8 @@ namespace Kata
                 return 0;
             }
 
-            return int.Parse(userInput);
+            var tok = userInput.Split(",").Select(int.Parse).ToArray();
+            return tok[0] + (tok.Count()>1 ? tok[1] :0);
         }
     }
 }
