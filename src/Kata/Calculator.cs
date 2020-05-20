7ca2504ab54b5d5a6dc30f9239bee7799394076a
Ricardo Zelaya <ricardo.zelaya@acklenavenue.com>
2020-05-20 14:50:11 -0600

    Test Escenario 5 katta calculator

--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -9,10 +9,6 @@ public  class Calculator
             return 0;
 
         var numbers = data.Split(",").Select(int.Parse);
-        if (numbers.Count() == 1)
-        {
-            return numbers.First();
-        }
-        return numbers.First() + numbers.Last();
+        return numbers.Sum();
     }
 }
