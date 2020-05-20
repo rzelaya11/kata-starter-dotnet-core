a598831550acaba93f77d37c2c073840f1e7fba7
Ricardo Zelaya <ricardo.zelaya@acklenavenue.com>
2020-05-20 13:51:03 -0600

    Test escenario number 3

--- src/Kata.Spec/when_feeding_the_monkey.cs
+++ src/Kata.Spec/when_feeding_the_monkey.cs
@@ -62,6 +62,19 @@ public class when_input_Two_numbers
     private static int _result;
 }
 // 4. Given the user input is an unknown amount of numbers when calculating the sum then it should return the sum of all the numbers. (example "1,2,3" should equal 6)
+public class when_Having_several_numbers
+{
+    Establish _context = () =>
+    {
+        _systemUnderTest = new Calculator();
+    };
+
+    Because of = () => { _result = _systemUnderTest.Sum("1,2,3"); };
+
+    It should_Return_the_sum_of_all = () => { _result.Should().Be(6); };
+    private static Calculator _systemUnderTest;
+    private static int _result;
+}
 // 5. Given the user input is multiple numbers with new line and comma delimiters when calculating the sum then it should return the sum of all the numbers. (example "1\n2,3" should equal 6)
 // 6. Given the user input is multiple numbers with a custom single-character delimiter when calculating the sum then it should return the sum of all the numbers. (example “//;\n1;2” should return 3)
 // 7. Given the user input contains one negative number when calculating the sum then it should throw an exception "negatives not allowed: x" (where x is the negative number).
--- src/Kata/Calculator.cs
+++ src/Kata/Calculator.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Globalization;
+using System.Linq;
 using System.Numerics;
 
 public partial class Calculator
@@ -6,7 +8,9 @@ public partial class Calculator
     public int Sum(string data)
     {
         if(data =="" )return 0;
+
+        var numbers = data.Split(',').Select(Int32.Parse).ToArray();
         
-        return Int32.Parse(data);
+        return numbers[0] + (numbers.Length>1 ? numbers[1] : 0);
     }
 }
